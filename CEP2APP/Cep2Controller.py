22e5706dfc6bdd0552d91a7becb6dcb0e8e8ec99
PI <andreasgbpoulsen@gmail.com>
2024-05-16 14:34:26 +0200

    Done

--- CEP2APP/Cep2Controller.py
+++ CEP2APP/Cep2Controller.py
@@ -1,6 +1,5 @@
 from hmac import new
 from itertools import count
-from arrow import now
 from Cep2Model import Cep2Model
 from Cep2WebClient import Cep2WebClient, Cep2WebDeviceEvent
 from Cep2Zigbee2mqttClient import (Cep2Zigbee2mqttClient,
@@ -10,64 +9,65 @@ from datetime import datetime, timedelta
 from threading import Thread
 
 class Cep2Controller:
-    HTTP_HOST = "172.20.10.6"
+    HTTP_HOST = "http://172.20.10.6/receive_data.php"  # Replace with your server's IP
+    HTTP_HOST_RETRIEVE = "http://172.20.10.6/retrieve_variables.php"
     MQTT_BROKER_HOST = "localhost"
     MQTT_BROKER_PORT = 1883
-    medicationTime = datetime(2024, 5, 16, 8, 44)
+    medicationTime = datetime(2024, 5, 16, 16, 18)
     timeWindowBefore = 1
     timeWindowAfter = 1
-    dailyUpdateTime = datetime(2024, 5, 16, 23, 59)
+    dailyUpdateTime = datetime(2024, 5, 16, 14, 13)
     currentRoom = "Stue"
     runningThread = True
     medicationTaken = False
-    sensorToActuator = {"bedroom": "glowyBoi", "livingroom": "livingroomLight"}
-
-
-    """ The controller is responsible for managing events received from zigbee2mqtt and handle them.
-    By handle them it can be process, store and communicate with other parts of the system. In this
-    case, the class listens for zigbee2mqtt events, processes them (turn on another Zigbee device)
-    and send an event to a remote HTTP server.
-    """
+    HeucodNum = 0
+    EventDescription = ""
+    sensorToActuator = {
+        "bedRoom": "bedroomLight",
+        "livingRoom": "livingroomLight",
+    }
 
     def __init__(self, devices_model: Cep2Model) -> None:
-        """ Class initializer. The actuator and monitor devices are loaded (filtered) only when the
-        class is instantiated. If the database changes, this is not reflected.
-
-        Args:
-            devices_model (Cep2Model): the model that represents the data of this application
-        """
         self.__devices_model = devices_model
         self.__z2m_client = Cep2Zigbee2mqttClient(host=self.MQTT_BROKER_HOST,
                                                   port=self.MQTT_BROKER_PORT,
                                                   on_message_clbk=self.__zigbee2mqtt_event_received)
 
     def timeLoop(self):
-        while(self.runningThread):
+        while self.runningThread:
             nowTime = datetime.now()
-            if(nowTime > self.dailyUpdateTime):
-                client = Cep2WebClient(self.HTTP_HOST)
+            if nowTime > self.dailyUpdateTime:
+                client = Cep2WebClient(self.HTTP_HOST_RETRIEVE)
                 var1, self.timeWindowBefore, self.timeWindowAfter = client.retrieve_variables()
                 self.medicationTime = datetime(2024, 5, 16, var1[0], var1[1])
+                print(self.timeWindowBefore)
                 self.medicationTaken = False
                 self.dailyUpdateTime = self.dailyUpdateTime + timedelta(days=1)
 
-            if(nowTime > self.medicationTime + timedelta(minutes=self.timeWindowAfter)):
-                self.__z2m_client.change_state(self.sensorToActuator[self.currentRoom], "ON", 0, 0)
-                self.__z2m_client.change_state("kitchenLight", "ON", 0.15, 0.75)
+            if nowTime > self.medicationTime + timedelta(minutes=self.timeWindowAfter):
+                if self.currentRoom in self.sensorToActuator:
+                    self.__z2m_client.change_state(self.sensorToActuator[self.currentRoom], "ON", 0.7, 0.29)
+                else:
+                    print(f"Unknown room: {self.currentRoom}")
+                self.__z2m_client.change_state("kitchenLight", "ON", 0.7, 0.29)
 
-            elif(nowTime > self.medicationTime): 
-                self.__z2m_client.change_state(self.sensorToActuator[self.currentRoom], "ON", 0.15, 0.75)
-                self.__z2m_client.change_state("kitchenLight", "ON", 0.15, 0.75)
+            elif nowTime > self.medicationTime:
+                if self.currentRoom in self.sensorToActuator:
+                    self.__z2m_client.change_state(self.sensorToActuator[self.currentRoom], "ON", 0.45, 0.5)
+                else:
+                    print(f"Unknown room: {self.currentRoom}")
+                self.__z2m_client.change_state("kitchenLight", "ON", 0.45, 0.5)
 
-            elif(nowTime > self.medicationTime - timedelta(minutes=self.timeWindowBefore)):
-                self.__z2m_client.change_state(self.sensorToActuator[self.currentRoom], "ON", 0.15, 0.75)
+            elif nowTime > self.medicationTime - timedelta(minutes=self.timeWindowBefore):
+                if self.currentRoom in self.sensorToActuator:
+                    self.__z2m_client.change_state(self.sensorToActuator[self.currentRoom], "ON", 0.15, 0.75)
+                else:
+                    print(f"Unknown room: {self.currentRoom}")
                 self.__z2m_client.change_state("kitchenLight", "ON", 0.15, 0.75)
 
             time.sleep(5) # Delay 5 seconds
 
     def start(self) -> None:
-        """ Start listening for zigbee2mqtt events.
-        """
         self.__z2m_client.connect()
         timeThread = Thread(target=self.timeLoop)
         timeThread.start()
@@ -76,7 +76,7 @@ class Cep2Controller:
 
     def blink(self, light_sensor_id) -> None:
         counter = 0
-        while(counter < 5):
+        while counter <= 1:
             self.__z2m_client.change_state(light_sensor_id, "ON", 0.7, 0.28)
             time.sleep(1)
             self.__z2m_client.change_state(light_sensor_id, "OFF", 0.7, 0.28)
@@ -85,94 +85,82 @@ class Cep2Controller:
         return
 
     def stop(self) -> None:
-        """ Stop listening for zigbee2mqtt events.
-        """
         self.__z2m_client.disconnect()
 
     def __zigbee2mqtt_event_received(self, message: Cep2Zigbee2mqttMessage) -> None:
-        """ Process an event received from zigbee2mqtt. This function given as callback to
-        Cep2Zigbee2mqttClient, which is then called when a message from zigbee2mqtt is received.
-
-        Args:
-            message (Cep2Zigbee2mqttMessage): an object with the message received from zigbee2mqtt
-        """
-        # If message is None (it wasn't parsed), then don't do anything.
+        occupancy = False
         if not message:
             return
 
         print(
             f"zigbee2mqtt event received on topic {message.topic}: {message.data}")
 
-        # If the message is not a device event, then don't do anything.
         if message.type_ != Cep2Zigbee2mqttMessageType.DEVICE_EVENT:
             return
 
-        # Parse the topic to retreive the device ID. If the topic only has one level, don't do
-        # anything.
         tokens = message.topic.split("/")
         if len(tokens) <= 1:
             return
 
-        # Retrieve the device ID from the topic.
         device_id = tokens[1]
 
-        # If the device ID is known, then process the device event and send a message to the remote
-        # web server.
         device = self.__devices_model.find(device_id)
 
         new_update = False
-        if(device_id == "vibratingBoi"):
+        if device_id == "pillboxSensor":
             vibration = message.event["vibration"]
             nowTime = datetime.now()
-            if vibration and nowTime > self.medicationTime - self.timeWindowBefore:
-                self.__z2m_client.change_state(self.sensorToActuator[self.currentRoom], "OFF", 0, 0)
+            if vibration and nowTime > self.medicationTime - timedelta(minutes=self.timeWindowBefore) and self.medicationTaken == False:
                 self.__z2m_client.change_state("kitchenLight", "OFF", 0, 0)
+                self.__z2m_client.change_state(self.sensorToActuator[self.currentRoom], "OFF", 0, 0)
                 self.medicationTime = self.medicationTime + timedelta(days=1)
                 self.runningThread = True
                 self.medicationTaken = True
-                web_event = Cep2WebDeviceEvent(device_id=device.id_,
-                                               device_type=device.type_,
-                                               measurement="Medication taken",
-                                               heucod_event=82295)
+                self.HeucodNum = 82295
+                self.EventDescription = "Medication taken"
+                print("Medication has been taken")
                 new_update = True
 
-            elif vibration and (nowTime < (self.medicationTime - self.timeWindowBefore) or self.medicationTaken == True):
-                self.blink("kitchenLight")
-                web_event = Cep2WebDeviceEvent(device_id=device.id_,
-                                               device_type=device.type_,
-                                               measurement="Pillbox moved outside window",
-                                               heucod_event=81493)
+            elif vibration:
+                self.HeucodNum = 81493
+                self.EventDescription = "Pillbox moved outside window"
+                print("ERROR")
                 new_update = True
+                self.blink("kitchenLight")
                 
-        if device:
-            try:
-                occupancy = message.event["occupancy"]
-            except KeyError:
-                pass
+
+        if device_id == "bedRoom" or device_id == "livingRoom":
+            occupancy = message.event["occupancy"]
+        
+        
+        if occupancy:
+            nowTime = datetime.now()
+            if device_id != self.currentRoom and not self.medicationTaken and nowTime > self.medicationTime - timedelta(minutes=self.timeWindowBefore):
+                self.__z2m_client.change_state(self.sensorToActuator[self.currentRoom], "OFF", 0, 0)
+                self.__z2m_client.change_state(self.sensorToActuator[device_id], "ON", 0.15, 0.75)
+                self.currentRoom = device_id
+                new_update = True
+                print(f"Current room is {self.currentRoom}")
             else:
-                # Based on the value of occupancy, change the state of the actuators to ON
-                # (occupancy is true, i.e. a person is present in the room) or OFF.
-                nowTime = datetime.now()
-                if(device_id != self.currentRoom and self.medicationTaken == False and occupancy and nowTime > self.medicationTime - timedelta(minutes=self.timeWindowBefore)):
-                    self.__z2m_client.change_state(self.sensorToActuator[self.currentRoom], "OFF", 0, 0)
-                    self.__z2m_client.change_state(self.sensorToActuator[device_id], "ON", 0.15, 0.75)
-                    self.currentRoom = device_id
-                    new_update = True
-                    print(f"Current room is {self.currentRoom}")
-                elif(device_id != self.currentRoom and occupancy):
-                    self.currentRoom = device_id
-                    new_update = True
-                    print(f"Current room is {self.currentRoom}")
-                # Register event in the remote web server.
-                web_event = Cep2WebDeviceEvent(device_id=device.id_,
-                                               device_type=device.type_,
-                                               measurement=occupancy,
-                                               heucod_event=82099)
-        if(new_update):
+                self.currentRoom = device_id
+                new_update = True
+                print(f"Current room is {self.currentRoom}")
+            self.HeucodNum = 82099
+            self.EventDescription = occupancy
+            new_update = True
+
+
+
+
+        if new_update:
+            print("sending event")
+            web_event = Cep2WebDeviceEvent(device_id=device.id_,
+                                            device_type=device.type_,
+                                            measurement=self.EventDescription,
+                                            heucod_event=self.HeucodNum)
             client = Cep2WebClient(self.HTTP_HOST)
-            new_update = False
             try:
-                client.send_event(web_event.to_heucod())
+                status_code = client.send_event(web_event.to_heucod())
+                print(f"Status code: {status_code}")
             except ConnectionError as ex:
                 print(f"{ex}")
-        
--- CEP2APP/Cep2WebClient.py
+++ CEP2APP/Cep2WebClient.py
@@ -1,85 +1,50 @@
 import json
 from dataclasses import dataclass
 from typing import Any
-from Cep2Heucod import HeucodEvent
+from Cep2Heucod import HeucodEvent, HeucodEventJsonEncoder
 import requests
 from datetime import datetime
 
-
 @dataclass
 class Cep2WebDeviceEvent:
-    """ Represents a device event that is sent to the remote web service.
-    """
     device_id: str
     device_type: str
     measurement: Any
     heucod_event: int
 
     def to_heucod(self) -> str:
-
         event_heucod = HeucodEvent()
         event_heucod.id = self.device_id
-        event_heucod.event_type = str(self.heucod_event)
         event_heucod.event_type_enum = self.heucod_event
         event_heucod.description = self.measurement
         event_heucod.timestamp = datetime.now().isoformat()
         event_heucod.device_model = self.device_type
 
-
         return event_heucod.to_json()
 
-
 class Cep2WebClient:
-    """ Represents a local web client that sends events to a remote web service.
-    """
-
     def __init__(self, host: str) -> None:
-        """ Default initializer.
-
-        Args:
-            host (str): an URL with the address of the remote web service
-        """
         self.__host = host
 
     def send_event(self, event: str) -> int:
-        """ Sends a new event to the web service.
-
-        Args:
-            event (str): a string with the event to be sent.
-
-        Raises:
-            ConnectionError: if the connection to the web service fails
-
-        Returns:
-            int: the status code of the request
-        """
         try:
-            # A new event is sent as an HTTP POST request.
-            response = requests.post(self.__host, data=event)
+            headers = {'Content-Type': 'application/json'}
+            response = requests.post(self.__host, data=event, headers=headers)
 
             return response.status_code
         except requests.exceptions.ConnectionError:
             raise ConnectionError(f"Error connecting to {self.__host}")
     
     def retrieve_variables(self) -> tuple:
-        """ Retrieves three variables from the PHP server.
-
-        Returns:
-            tuple: a tuple containing three variables retrieved from the server
-        """
         try:
-            # Send a GET request to retrieve the variables from the server
             response = requests.get(self.__host)
-
-            # Parse the response and extract the variables
             data = response.json()
-            var1 = data.get('variable1')
-            var2 = data.get('variable2')
-            var3 = data.get('variable3')
+            var1 = data.get('variable1', [0, 0])  # Default to [0, 0] if not found
+            var2 = data.get('variable2', 0)  # Default to 0 if not found
+            var3 = data.get('variable3', 0)  # Default to 0 if not found
 
             return var1, var2, var3
         except requests.exceptions.ConnectionError:
             raise ConnectionError(f"Error connecting to {self.__host}")
         except Exception as e:
-            # Handle any other exceptions gracefully
             raise RuntimeError(f"Error retrieving variables: {e}")
